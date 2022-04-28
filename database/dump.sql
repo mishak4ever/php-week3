5617d6b402629250700198f45c66f07d6681b975
Михаил <mishak4ever@gmail.com>
2022-04-28 11:17:12 +0500

    SQL dump

--- database/dump.sql
+++ database/dump.sql
@@ -38,7 +38,7 @@ CREATE TABLE `messages` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `image` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (13,9,'Новое сообщение','2022-04-21 12:28:17',NULL),(28,10,'fwefsdg','2022-04-21 14:25:11','ff30e7c1c7baa5975fad7920406e5b2c'),(29,10,'sdaasdas','2022-04-21 14:31:58',NULL),(30,10,'dsfdf','2022-04-21 14:43:00',NULL),(31,10,'asdfsdf','2022-04-21 14:43:03',NULL),(32,10,'adfsdf','2022-04-21 14:43:06',NULL),(33,10,'sdgetrhfgh','2022-04-21 14:43:10',NULL),(36,10,'Привет','2022-04-21 14:58:33','1529bdffe03e9444533985580b600142'),(37,10,'Hello','2022-04-21 15:01:24',NULL),(40,10,'wqwe','2022-04-21 15:09:03','f01b99b1b13c47a0e1c03081b075c014');
+INSERT INTO `messages` VALUES (29,10,'sdaasdas','2022-04-21 14:31:58',NULL),(66,20,'sdfdsf','2022-04-28 10:27:56','093c698a216a5cd52cc69a1e22d2845c'),(67,20,'asd','2022-04-28 10:28:30',NULL),(75,20,'sdf','2022-04-28 10:48:18','70cfb787ae2b17496d09ae6206131a92'),(76,20,'Hello','2022-04-28 10:59:23','a9fbf8d114de2d25fb0eb190458cd19d');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,8 +66,9 @@ CREATE TABLE `users` (
   `password` varchar(245) DEFAULT NULL,
   `gender` tinyint(1) DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `ava` varchar(145) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,7 +77,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (4,'vova','','mail.ru','6f1e2178beb8732ea3d0b4a29b14f5cb1c8c706d',NULL,'2022-04-20 21:50:42'),(8,'mike','','mike@mail.ru','1aa2c5339dc5686ed9a0dd7652660f465d881cbd',NULL,'2022-04-20 22:23:39'),(9,'olga','','olga@mail.ru','2222',NULL,'2022-04-20 22:25:42'),(10,'Mike','','mike2@mail.ru','c3a0a2a317f929477ea0ecad91bdaad2030bf46b',NULL,'2022-04-21 11:37:49');
+INSERT INTO `users` VALUES (4,'vova','','mail.ru','6f1e2178beb8732ea3d0b4a29b14f5cb1c8c706d',NULL,'2022-04-20 21:50:42',NULL),(8,'mike','','mike@mail.ru','1aa2c5339dc5686ed9a0dd7652660f465d881cbd',NULL,'2022-04-20 22:23:39',NULL),(9,'olga','','olga@mail.ru','2222',NULL,'2022-04-20 22:25:42',NULL),(10,'Mike','','mike2@mail.ru','c3a0a2a317f929477ea0ecad91bdaad2030bf46b',NULL,'2022-04-21 11:37:49','107f693e554c25b42f293873e6221669'),(11,'Василий','','vas@mail.ru','c30ba986c346dbebf510257c4ef365454f834d7c',NULL,'2022-04-21 15:13:54',NULL),(12,'Василий','','head@amblogic.ru','312ef5bd9493a1bda4c47d64746b0317888341d8',NULL,'2022-04-27 14:56:35',NULL),(20,'Михаил','','mishak4ever@gmail.com','809a05a685562c0fac72e826e1eb4ae930ab8450',NULL,'2022-04-27 17:43:41','83df195fed3d40b899d988a7d251f73b');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50112 SET @disable_bulk_load = IF (@is_rocksdb_supported, 'SET SESSION rocksdb_bulk_load = @old_rocksdb_bulk_load', 'SET @dummy_rocksdb_bulk_load = 0') */;
@@ -93,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-21 15:10:06
+-- Dump completed on 2022-04-28 11:15:57
